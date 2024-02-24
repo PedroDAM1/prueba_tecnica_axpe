05ca7b3d414caf727a97002bf829342232a8cd4c
Pedro <pedro.pelayo003@gmail.com>
2024-02-24 22:54:36 +0100

    Implementado sistema de cache en el repositorio

--- app/src/main/java/com/pedropelayo/prueba_tecnica/data/repositories/UserRepositoryImpl.kt
+++ app/src/main/java/com/pedropelayo/prueba_tecnica/data/repositories/UserRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.pedropelayo.prueba_tecnica.data.repositories
 import com.pedropelayo.prueba_tecnica.data.remote.mapper.ResponseMapper
 import com.pedropelayo.prueba_tecnica.data.remote.mapper.UserMapper
 import com.pedropelayo.prueba_tecnica.data.remote.model.user.ApiResponseInfo
+import com.pedropelayo.prueba_tecnica.data.remote.model.user.UserApiModel
 import com.pedropelayo.prueba_tecnica.data.remote.services.UserService
 import com.pedropelayo.prueba_tecnica.data.utils.AppDispatchers
 import com.pedropelayo.prueba_tecnica.data.utils.Dispatcher
@@ -25,18 +26,29 @@ class UserRepositoryImpl @Inject constructor (
     @Dispatcher(AppDispatchers.IO) private val ioDispatcher : CoroutineDispatcher
 ) : UserRepository {
 
+    private val userCache = mutableSetOf<UserModel>()
 
     override fun getUsers(page: Int): Flow<DataResponse<List<UserModel>>> = flow {
-        when(val response = fetchUsers(page)){
+        val responseApi = fetchUsers(page)
+        emit(mapUsers(responseApi))
+    }.flowOn(Dispatchers.IO)
+
+    private fun mapUsers(responseApi : DataResponse<ApiResponseInfo>) : DataResponse<List<UserModel>>{
+        return when(responseApi){
             is DataResponse.Success -> {
                 //Si la api devuelve una respuesta correcta, la mmapeamos a nuestra capa de dominio
-                val usersDomain = response.data.results.map { userMapper.map(it) }
-                emit(DataResponse.Success(usersDomain))
+                val domainList = responseApi.data.results.mapNotNull { userApi ->
+                    val userDomain = userMapper.map(userApi)
+                    //Si el usuario ya lo hemos recuperado, entonces  no lo volvemos a devolver
+                    if(userCache.contains(userDomain)) null
+                    else userDomain
+                }
+                DataResponse.Success(domainList)
             }
-            //Si la api devuelve un error lo emitiremmos de vuelta
-            is DataResponse.Error -> emit(DataResponse.Error(response.errorType))
+            //Si devuelve una respuesta incorrecta la retornamos
+            is DataResponse.Error -> DataResponse.Error(responseApi.errorType)
         }
-    }.flowOn(Dispatchers.IO)
+    }
 
     /**
      * Hace un fetching de datos a la api y devuelve un wrapper de la respuesta
