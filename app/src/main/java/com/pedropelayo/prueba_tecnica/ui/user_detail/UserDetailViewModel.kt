35dcd1d777815c2f3ef2490ca19ed9f5c90eca26
Pedro <pedro.pelayo003@gmail.com>
2024-03-02 21:37:10 +0100

    Adaptado el viewmodel de user detail para recuperar datos de usuario

--- app/src/main/java/com/pedropelayo/prueba_tecnica/ui/user_detail/UserDetailViewModel.kt
+++ app/src/main/java/com/pedropelayo/prueba_tecnica/ui/user_detail/UserDetailViewModel.kt
@@ -4,14 +4,46 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pedropelayo.prueba_tecnica.domain.model.DataResponse
+import com.pedropelayo.prueba_tecnica.domain.model.ErrorType
+import com.pedropelayo.prueba_tecnica.domain.model.UserModel
+import com.pedropelayo.prueba_tecnica.domain.repositories.UserRepository
 import com.pedropelayo.prueba_tecnica.ui.user_detail.state.UserDetailState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class UserDetailViewModel : ViewModel() {
+@HiltViewModel
+class UserDetailViewModel @Inject constructor(
+    private val repository: UserRepository
+) : ViewModel() {
 
     var state: UserDetailState by mutableStateOf(UserDetailState.Loading)
 
     fun loadUser(uuid : String){
+        viewModelScope.launch {
+            repository.getUserByUuid(uuid).collect{ response ->
+                when (response) {
+                    is DataResponse.Error -> handleErrorState(response.errorType)
+                    is DataResponse.Success -> handleSuccessState(response.data)
+                }
+            }
+        }
+    }
 
+    private fun handleSuccessState(data: UserModel) {
+        state = UserDetailState.Success(data)
+    }
+
+    private fun handleErrorState(error: ErrorType) {
+        val message = when (error) {
+            ErrorType.NotFound -> "No se ha podido encontrar el usuario"
+            is ErrorType.UnknowError -> "Ocurrrió un error desconocido"
+        }
+
+        state = UserDetailState.Error(message)
     }
 
 }
